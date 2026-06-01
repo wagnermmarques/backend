11d78decbaf1eb772644582839f7a544094e00fd
Wagner Marques <wagner.wmarques@gmail.com>
2026-06-01 10:45:18 -0300

    Limpando todo o conteudo do repositorio

--- jukeboxd2.0/src/pages/PrivateRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-export function PrivateRoute({ children }) {
-  const user = localStorage.getItem('authUser');
-  return user ? children : <Navigate to="/entre" replace />;
-}
